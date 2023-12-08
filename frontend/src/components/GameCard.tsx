de686ea2c288af44ab12eac44bad677465d60c44
Maksim Murza <maksim.murza.v@gmail.com>
2023-12-08 13:53:01 +0400

    project structure, move shared deps up

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -11,33 +11,33 @@ import TimeLabel from './TimeLabel';
 import DateLabel from './DateLabel';
 
 const GameCard: FC<PropsWithChildren<FootballMatch & { className?: string }>> = ({
-  status,
-  utcDate,
-  homeTeam,
-  awayTeam,
-  leagueLogo,
-  className = '',
+	status,
+	utcDate,
+	homeTeam,
+	awayTeam,
+	leagueLogo,
+	className = '',
 }) => {
-  const { date, time } = prettifyDate(utcDate);
+	const { date, time } = prettifyDate(utcDate);
 
-  return (
-    <div className={`p-4 bg-white ` + className}>
-      <div className="flex gap-2">
-        <DateLabel>{date}</DateLabel>
-        <TimeLabel>{time}</TimeLabel>
-      </div>
-      <div className="flex justify-center items-center">
-        <TeamName align="right">{homeTeam.name}</TeamName>
-        <img className="w-24" src={homeTeam.logo} alt="Home team logo" />
-        <span className="mx-8"> – </span>
-        <img className="w-24" src={awayTeam.logo} alt="Home team logo" />
-        <TeamName>{awayTeam.name}</TeamName>
-      </div>
-      <div className="flex justify-end">
-        <img className="w-12" src={leagueLogo} alt="League logo" />
-      </div>
-    </div>
-  );
+	return (
+		<div className={`p-4 bg-white ` + className}>
+			<div className="flex gap-2">
+				<DateLabel>{date}</DateLabel>
+				<TimeLabel>{time}</TimeLabel>
+			</div>
+			<div className="flex justify-center items-center">
+				<TeamName align="right">{homeTeam.name}</TeamName>
+				<img className="w-24" src={homeTeam.logo} alt="Home team logo" />
+				<span className="mx-8"> – </span>
+				<img className="w-24" src={awayTeam.logo} alt="Home team logo" />
+				<TeamName>{awayTeam.name}</TeamName>
+			</div>
+			<div className="flex justify-end">
+				<img className="w-12" src={leagueLogo} alt="League logo" />
+			</div>
+		</div>
+	);
 };
 
 export default GameCard;
