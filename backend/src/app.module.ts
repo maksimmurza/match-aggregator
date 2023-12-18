91a1ce83c5b6293c5a36ef220a5fa9723634fd0b
Maksim Murza <maksim.murza.v@gmail.com>
2023-12-18 18:29:45 +0400

    Authorization module implementation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthorizationModule } from './authorization/authorization.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthorizationModule, ConfigModule.forRoot()],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
