086e189c111f99c4fad8f1105bb2323d04cb7d4b
Maksim Murza <maksim.murza.v@gmail.com>
2024-01-31 02:55:02 +0400

    move port to env var

--- backend/src/databaseConfig.ts
+++ backend/src/databaseConfig.ts
@@ -12,7 +12,7 @@ type DatabaseConfig = Pick<
 const productionConfig: DatabaseConfig = {
 	dialect: 'postgres',
 	host: process.env.DATABASE_HOST,
-	port: 5430,
+	port: Number(process.env.DATABASE_PORT),
 	username: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
